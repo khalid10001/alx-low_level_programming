a397553ee4063d138c27ad221d9a9f6644efd3b9
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-10 08:40:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entty point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(){
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
