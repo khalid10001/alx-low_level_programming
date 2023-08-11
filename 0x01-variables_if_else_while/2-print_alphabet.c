2f8158cb8cb8be3bae419b25089fcd5e415dc63e
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-11 08:03:36 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: print alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
