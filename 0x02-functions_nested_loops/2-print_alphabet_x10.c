d987cadfe85a6c13faa3a807aac167f0e9c633a5
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-15 13:23:19 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a - z alphabet in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha < 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
+
