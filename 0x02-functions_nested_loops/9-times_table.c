7a8ca9b2b1544b88303e1e531b3c2799cfe11130
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-15 16:25:27 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 			z = x * y;
 
 			if (z <= 9)
-			{	
+			{
 				_putchar(' ');
 			}
 			else
@@ -26,7 +26,6 @@ void times_table(void)
 				_putchar((z / 10) + 48);
 			}
 				_putchar((z % 10) + 48);
-			
 		}
 		_putchar('\n');
 	}
