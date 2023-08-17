d89d5063ef050abe0e9c0eea525c250a446dde78
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-17 10:06:46 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print the numbers from 1 to 100
+ *	Fizz is for divisible 3 and Buzz for divisible 5
+ *	and 15 is for both 3 and 5
  *
  * Return: Always 0 (Success)
  */
@@ -13,21 +15,28 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+		printf("\n");
 		}
 	}
-	printf("\n");
 	return (0);
 }
