0830d36fd09deea0870c8c0b052800cec0bbf7b5
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-28 15:12:16 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: pointer character variable
+ * @needle: pointer character variable
+ * Return: 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	while (haystack[i] != '\0')
+	{
+		int j = 0;
+		while (haystack[i] == needle[j] && needle[j] != '\0')
+		{
+			return (&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
