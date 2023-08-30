c94741251a84369382722702df5061ea8d9fa504
khalid10001 <khalid.2002.2011@gmail.com>
2023-08-30 14:50:39 -0700

    Power Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,6 +10,8 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int r;
+
 	if (y < 0)
 	{
 		return (-1);
@@ -24,7 +26,7 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		int r = x * _pow_recursion(x, y - 1);
+		r = x * _pow_recursion(x, y - 1);
 		return (r);
 	}
 }
