fb207f060104e960f7f40810982fa79e220d0497
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-11 12:03:42 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,11 @@
 
 /**
  * struct dog - a struct dog
+ *
  * @name: dog name
  * @float: dog age
  * @owner: dog owner name
+ *
  * Description: To store information about the dog
  *	name, age, owner name
  */
