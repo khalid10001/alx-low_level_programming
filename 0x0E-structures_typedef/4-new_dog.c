af4cc03bb79a284305787c2f4390bae5318e98f7
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-11 14:39:09 -0700

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,37 +10,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *nd;
+	dog_t *nd;
 
-        if (name == NULL || age < 0 || owner == NULL)
-        {
-                return ('\0');
-        }
+	if (name == NULL || age < 0 || owner == NULL)
+ 	{
+		return ('\0');
+	}
 
-        nd = malloc(sizeof(dog_t));
-        if (nd == NULL)
-        {
-                return ('\0');
-        }
+	nd = malloc(sizeof(dog_t));
+	if (nd == NULL)
+	{
+		return ('\0');
+	}
+	
+	nd->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if(nd->name == NULL)
+	{
+		free(nd);
+		return ('\0');
+	}
+	
+	nd->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if(nd->owner == NULL)
+	{
+		free(nd->name);        
+		free(nd);
+		return ('\0');
+	}
 
-        nd->name = malloc(sizeof(char) * strlen(name) + 1);
-        if(nd->name == NULL)
-        {
-                free(nd);
-                return ('\0');
-        }
-
-        nd->owner = malloc(sizeof(char) * strlen(owner) + 1);
-        if(nd->owner == NULL)
-        {
-                free(nd->name);
-                free(nd);
-                return ('\0');
-        }
-
-        nd->name = strcpy(nd->name, name);
-        nd->age = age;
-        nd->owner = strcpy(nd->owner, owner);
-
+	nd->name = strcpy(nd->name, name);
+	nd->age = age;
+	nd->owner = strcpy(nd->owner, owner);
 	return (nd);
 }
