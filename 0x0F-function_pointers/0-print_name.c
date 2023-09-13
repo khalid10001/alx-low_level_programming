f26749328df4b85683efbf5231a20bcb33491946
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-13 02:36:01 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: pointer character name
+ * @f: pointer function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+		f(name);
+}
