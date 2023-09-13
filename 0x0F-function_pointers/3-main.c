6dc1ea2624802d765e996ceab9bbb6e3b2a74dbf
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-13 15:49:38 -0700

    A program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
