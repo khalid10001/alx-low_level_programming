5814dbf6bec5e0c94c3297bdc9b8e85b26ffa274
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-26 11:55:04 -0700

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,14 +4,14 @@
  * free_listint - a function that frees a list
  * @head: pointer head
  * Return: 0.
-*/
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *nd;
 
 	while (head != NULL)
 	{
-		nd = (*head).next;
+		nd = head->next;
 		free(nd);
 	}
 }
