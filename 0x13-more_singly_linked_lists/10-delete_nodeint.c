354e135e2288baf7b47f5ad1e1432b3ae8c773bc
khalid10001 <khalid.2002.2011@gmail.com>
2023-09-26 15:05:11 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			prev_nd->next = nd->next;
 			free(nd);
-			return (-1);
+			return (1);
 		}
 		prev_nd = nd;
 		nd = nd->next;
