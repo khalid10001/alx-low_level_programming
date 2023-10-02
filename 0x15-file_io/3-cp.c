b9250f7b9a64231e45b36cb872fd0d4d13261329
khalid10001 <khalid.2002.2011@gmail.com>
2023-10-02 15:37:38 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-#define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE %s\n"
-#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
-#define ERR_NOCLOSE "Error: Can't close fd FD_VALUE %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
@@ -19,28 +15,35 @@ int main(int argc, char *argv[])
 	char buffer[READ_BUF_SIZE];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
+	{
+	dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to"),
+		exit(97);
+	}
 	from_f = open(argv[1], O_RDONLY);
 	if (from_f == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
-	to_f = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		exit(98);
+	to_f = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (to_f == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+		exit(99);
 
 	while ((b = read(from_f, buffer, READ_BUF_SIZE)) > 0)
 	{
 		if (write(to_f, buffer, b) != b)
-		{
-			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
 	}
 	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		exit(98);
 	from_f = close(from_f);
 	to_f = close(to_f);
 	if (from_f)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_f), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_f),
+		exit(100);
 	if (to_f)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_f), exit(100);
-	return (1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_f),
+		exit(100);
+	return (EXIT_SUCCESS);
 }
